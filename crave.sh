f325d4c1310f2915421287bd6f4005eecdca69b5
Deependra Shrestha <36838592+dpenra@users.noreply.github.com>
2025-10-06 22:15:07 +0545

    Update crave.sh

--- crave.sh
+++ crave.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-rm -rf device/xiaomi/sunny device/xiaomi/sunny-kernel vendor/xiaomi/sunny
+rm -rf device/xiaomi/sunny
+rm -rf device/xiaomi/sunny-kernel
+rm -rf vendor/xiaomi/sunny
 git clone https://github.com/RisingOS-Revived-devices/device_xiaomi_sunny.git -b sixteen device/xiaomi/sunny
 git clone https://github.com/RisingOS-Revived-devices/device_xiaomi_sunny-kernel.git --depth 1 -b sixteen device/xiaomi/sunny-kernel
 git clone https://github.com/RisingOS-Revived-devices/vendor_xiaomi_sunny.git -b sixteen vendor/xiaomi/sunny
 . build/envsetup.sh
 Riseup sunny user
 Rise b
-
-
-
