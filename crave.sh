b69a0e1b3aea0c91f2c4b295545f561f338cd5c4
dpenra <36838592+dpenra@users.noreply.github.com>
2025-11-15 16:02:49 +0545

    Update crave.sh

--- crave.sh
+++ crave.sh
@@ -1,6 +1,7 @@
 
 repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/RisingOS-Revived/android -b sixteen
-repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
+# repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
+/opt/crave/resync.sh
 . build/envsetup.sh
 Riseup sunny user
 Rise b
