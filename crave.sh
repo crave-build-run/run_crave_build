2fdb180757acd643f70c10adacdad92772a7a41e
dpenra <36838592+dpenra@users.noreply.github.com>
2026-08-30 08:46:10 +0545

    Update crave.sh

--- crave.sh
+++ crave.sh
@@ -1,7 +1,18 @@
-
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/RisingOS-Revived/android -b sixteen
-# repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
+repo init --depth=1 -u https://github.com/RisingOS-Revived/android -b seventeen --git-lfs
 /opt/crave/resync.sh
+
+git clone https://github.com/RisingOS-Revived-devices/device_xiaomi_sunny.git -b 17 device/xiaomi/sunny
+
+git clone https://github.com/PixelOS-Devices/android_device_xiaomi_sunny-kernel.git -b seventeen device/xiaomi/sunny-kernel
+
+git clone https://gitlab.com/NoPrincessHere/proprietary_vendor_xiaomi_sunny.git -b seventeen vendor/xiaomi/sunny
+
+git clone https://github.com/PixelOS-AOSP/android_hardware_xiaomi.git -b seventeen hardware/xiaomi
+
+git clone https://github.com/KProfiles/android_packages_apps_KProfiles.git -b main packages/apps/KProfiles
+
+git clone https://github.com/cyberknight777/android_packages_apps_DisplayFeatures.git -b master packages/apps/DisplayFeatures
+
 . build/envsetup.sh
 Riseup sunny user
 Rise b
