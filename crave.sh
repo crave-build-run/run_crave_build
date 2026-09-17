c8626a5ae4a8d5a50db0f9168132e25c70e206dc
dpenra <36838592+dpenra@users.noreply.github.com>
2026-09-17 22:55:15 +0545

    Change clone URL for sunny-kernel repository
    Updated repository URLs for cloning in crave.sh.

--- crave.sh
+++ crave.sh
@@ -15,13 +15,15 @@ git reset --hard origin/seventeen
 git clean -fd
 cd ../../..
 
-git clone --depth 1 -b seventeen https://github.com/PixelOS-Devices/android_device_xiaomi_sunny-kernel.git device/xiaomi/sunny-kernel
+git clone --depth 1 -b seventeen https://github.com/RisingOS-Revived-devices/device_xiaomi_sunny-kernel.git device/xiaomi/sunny-kernel
 
 git clone --depth 1 -b seventeen https://github.com/PixelOS-AOSP/android_hardware_xiaomi.git hardware/xiaomi
 
 git clone --depth 1 -b main https://github.com/KProfiles/android_packages_apps_KProfiles.git packages/apps/KProfiles
 
 git clone --depth 1 -b master https://github.com/cyberknight777/android_packages_apps_DisplayFeatures.git packages/apps/DisplayFeatures
+
+git clone https://github.com/private-keys/vendor_lineage-priv_keys.git --depth 1 -b main vendor/lineage-priv/keys
 
 
 
