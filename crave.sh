0a1bee2cb730ab70adf302485815269027aef0a1
dpenra <36838592+dpenra@users.noreply.github.com>
2026-09-22 18:50:38 +0545

    Update crave.sh

--- crave.sh
+++ crave.sh
@@ -1,6 +1,6 @@
-repo init --depth=1 -u https://github.com/RisingOS-Revived/android -b seventeen --git-lfs
-/opt/crave/resync.sh
-repo sync -c --force-sync --force-remove-dirty --no-clone-bundle --no-tags
+# repo init --depth=1 -u https://github.com/RisingOS-Revived/android -b seventeen --git-lfs
+# /opt/crave/resync.sh
+# repo sync -c --force-sync --force-remove-dirty --no-clone-bundle --no-tags
 
 
 git clone -b seventeen https://github.com/RisingOS-Revived-devices/device_xiaomi_sunny.git device/xiaomi/sunny
